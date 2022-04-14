2ff0f23a4a0eebe442dfb67b5ec32edac1b43134
Nozcepni <53128374+Nozcepni@users.noreply.github.com>
2022-04-15 01:37:51 +0300

    this is an rest controller example

---
+++ src/main/java/com/letscodespringdemo/letscodehelloworld/LetsCodeHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.letscodespringdemo.letscodehelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsCodeHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsCodeHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letscodespringdemo/letscodehelloworld/LetsCodeHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscodespringdemo.letscodehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsCodeHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
